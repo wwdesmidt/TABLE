4e3d6cecfafb33736d759aae29ab0c88370ca258
Wade De Smidt <wade.desmidt@gmail.com>
2020-01-30 20:30:25 -0800

    replaced left/right mouse button functions with left button only and right button menu

--- table.py
+++ table.py
@@ -21,8 +21,30 @@ table_top = tk.Canvas(root, width = root.winfo_screenwidth(), height = root.winf
 table_top.pack()
 
 
+mode = "move"
 
+#start menu section
+###############################################################################################
 
+def set_draw_mode(): 
+    global mode
+    mode="draw"
+
+def set_move_mode():
+    global mode
+    mode="move"
+
+# create a popup menu
+menu = tk.Menu(root, tearoff=0)
+menu.add_command(label="Draw", command=set_draw_mode)
+menu.add_command(label="Move", command=set_move_mode)
+menu.add_command(label="Exit", command=root.quit)
+
+def popup(event):
+    menu.post(event.x_root, event.y_root)
+
+#end menu section
+###############################################################################################
 
 
 
@@ -35,13 +57,13 @@ table_top.pack()
 #maybe an image file + some json or xml with matching names?
 #distances in feet even for large maps (require units and add auto conversion?)
 
-map_image = Image.open("./sample_assets/sample_dungeon_map.jpg")
-map_width_feet = 105
-map_height_feet  = 135
+#map_image = Image.open("./sample_assets/sample_dungeon_map.jpg")
+#map_width_feet = 105
+#map_height_feet  = 135
 
-#map_image = Image.open("./sample_assets/sample_world_map.jpg")
-#map_width_feet = 9950000
-#map_height_feet  = 7550000
+map_image = Image.open("./sample_assets/sample_world_map.jpg")
+map_width_feet = 9950000
+map_height_feet  = 7550000
 
 
 
@@ -99,23 +121,6 @@ map_feet_per_pixel = ((map_height_feet /map_image.height) + (map_width_feet /map
 ###############################################################################################
 
 
-#print some instructions
-table_top.create_rectangle(
-    (root.winfo_screenwidth()/2)+150, 
-    (root.winfo_screenheight()/2)+75, 
-    (root.winfo_screenwidth()/2)-150, 
-    (root.winfo_screenheight()/2)-55,
-    fill="white", 
-    stipple="gray50"
-)
-
-table_top.create_text(root.winfo_screenwidth()/2, (root.winfo_screenheight()/2)-30, text=f"TABLE", font=("TkDefaultFont", 24))
-table_top.create_text(root.winfo_screenwidth()/2, (root.winfo_screenheight()/2)+15, text=f"left click to measure distance")
-table_top.create_text(root.winfo_screenwidth()/2, (root.winfo_screenheight()/2)+30, text=f"right click to draw")
-table_top.create_text(root.winfo_screenwidth()/2, (root.winfo_screenheight()/2)+45, text=f"double right click to clear drawing")
-table_top.create_text(root.winfo_screenwidth()/2, (root.winfo_screenheight()/2)+60, text=f"press escape to quit")
-
-
 
 
 #Start input section
@@ -133,22 +138,35 @@ drawing_start_y = 0
 def exit(event):
     root.quit()
 
-
-def mouse_move(event):
-    #delete the old position text and print the new positions
-    table_top.delete(table_top.find_withtag("mouse_position"))
-    table_top.create_text(root.winfo_screenwidth()/2, root.winfo_screenheight()/2, text=f"{event}", tag="mouse_position")
-
-
 def left_mouse_button_press(event):
+    if mode=="move":
+        left_mouse_button_press_move(event)
+    elif mode=="draw":
+        left_mouse_button_press_draw(event)
+
+def left_mouse_button_press_move(event):
+        #get the global variables for where we are starting and set them to where the left button was pressed
+        global draging_distance_start_x
+        global draging_distance_start_y
+        draging_distance_start_x = event.x
+        draging_distance_start_y = event.y
+
+def left_mouse_button_press_draw(event):
     #get the global variables for where we are starting and set them to where the left button was pressed
-    global draging_distance_start_x
-    global draging_distance_start_y
-    draging_distance_start_x = event.x
-    draging_distance_start_y = event.y
+    global drawing_start_x
+    global drawing_start_y
+    drawing_start_x = event.x
+    drawing_start_y = event.y
+
+
 
 def left_mouse_button_drag(event):
+    if mode=="move":
+        left_mouse_button_drag_move(event)
+    elif mode=="draw":
+        left_mouse_button_drag_draw(event)
 
+def left_mouse_button_drag_move(event):
     #calculate the distance between the starting point and the current point
     dist = math.sqrt((event.x - draging_distance_start_x)**2 + (event.y - draging_distance_start_y)**2)
     #add in calculation for feet per pixel
@@ -164,28 +182,43 @@ def left_mouse_button_drag(event):
         #miles with 1 decimal point
         dist = dist/5280
         distance_unit = "Miles"
-
-
-
-    dist = round(dist)
-
-    #string for distance unit
-    #change to miles on bigger maps?
-
-
+        dist = round(dist,1)
+ 
     #delete the old distance text and line and draw new ones
     table_top.delete(table_top.find_withtag("dragging_distance_line"))
     table_top.delete(table_top.find_withtag("dragging_distance_text"))
     table_top.create_line(draging_distance_start_x, draging_distance_start_y, event.x, event.y, tag = "dragging_distance_line")
     table_top.create_text(event.x+15, event.y, text=f"{dist} {distance_unit}", tag="dragging_distance_text", anchor="w")
 
-    #for dragging also call the normal movement handler
-    mouse_move(event)
+
+def left_mouse_button_drag_draw(event):
+    #get global variables for where the mouse is moving from
+    global drawing_start_x
+    global drawing_start_y
+    
+    #draw a line from where the mouse was last time it moved until now
+    table_top.create_line(drawing_start_x, drawing_start_y, event.x, event.y, tag="drawn_line")
+
+    #set the starting position for next time with the ending position this time
+    drawing_start_x = event.x
+    drawing_start_y = event.y
+
 
 def left_mouse_button_release(event):
+    if mode=="move":
+        left_mouse_button_release_move(event)
+    elif mode=="draw":
+        left_mouse_button_release_draw(event)
+
+def left_mouse_button_release_move(event):
     #the user let go of the left mouse button, delete the distance line and text
     table_top.delete(table_top.find_withtag("dragging_distance_line"))
     table_top.delete(table_top.find_withtag("dragging_distance_text"))
+
+def left_mouse_button_release_draw(event):
+    #dont need to do anything right now when the user lets go of the mouse button while drawing
+    pass
+
 
 
 def right_mouse_button_press(event):
@@ -208,24 +241,26 @@ def right_mouse_button_drag(event):
     drawing_start_y = event.y
 
     #for dragging also call the normal movement handler
-    mouse_move(event)
+    #mouse_move(event)
 
-def right_mouse_button_double_click(event):
-    #delete all the little line segments 
-    for line_segment in table_top.find_withtag("drawn_line"):
-        table_top.delete(line_segment)
 
-    
+def right_mouse_button_release(event):
+    #user right clicked, open menu
+    table_top.create_rectangle(event.x+20, event.y+20, event.x-20, event.y-20, tag="menu")
+
+#destroy every object with a certain tag
+def destroy_by_tag(tag):
+    for line_segment in table_top.find_withtag(tag):
+            table_top.delete(line_segment)
+
+
+
 
 #set up all the key bindings
-root.bind_all("<Escape>", exit)
-root.bind_all("<Motion>", mouse_move)
 root.bind_all("<ButtonPress-1>", left_mouse_button_press)
 root.bind_all("<B1-Motion>", left_mouse_button_drag)
 root.bind_all("<ButtonRelease-1>", left_mouse_button_release)
-root.bind_all("<ButtonPress-3>", right_mouse_button_press)
-root.bind_all("<B3-Motion>", right_mouse_button_drag)
-root.bind_all("<Double-Button-3>", right_mouse_button_double_click)
+root.bind_all("<ButtonRelease-3>", popup)
 
 #end map section
 ###############################################################################################
