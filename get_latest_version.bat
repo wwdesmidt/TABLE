e23f5053ffe7063f6ebc3f0f883ffdef1cafb421
Wade De Smidt <wade.desmidt@gmail.com>
2020-02-01 11:18:22 -0800

    updated get_latest_version.bat to also update pip and pip dependencies

--- get_latest_version.bat
+++ get_latest_version.bat
@@ -1,3 +1,15 @@
 @echo off
+
+@echo Making sure PIP is up to date
+python -m pip install --upgrade pip
+@echo.
+
+@echo Verifying PIP dependencies
+pip install pillow
+@echo.
+
+@echo Making sure TABLE is up to date
 git pull
+@echo.
+
 pause
