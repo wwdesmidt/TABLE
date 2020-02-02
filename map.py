d56d18037a8e7b8f4a225ef4e824bad09ed8ccee
Wade De Smidt <wade.desmidt@gmail.com>
2020-02-02 15:43:45 -0800

    load via image file instead of json. app handles creating json if needed and manages values. image and json linked by directory and file name

--- map.py
+++ map.py
@@ -1,36 +1,52 @@
 from PIL import Image, ImageTk
 import json
-from pathlib import Path, PurePath
+import pathlib
+import os
 
 class Map():
     def __init__(self, file_name, canvas):
 
-
-        #map files and height and width in feet
-        #eventually these will be bundled
-        #maybe an image file + some json or xml with matching names?
-        #distances in feet even for large maps (require units and add auto conversion?)
-
-        #map_image = Image.open("./sample_assets/sample_dungeon_map.jpg")
-        #map_width_feet = 105
-        #map_height_feet  = 135
-
+        #store the canvas that we will br drawing the map to locally
+        #as long as we never reassign it (self.canvas=whatever)
+        #we can use it as a reference
         self.canvas = canvas
 
-        #self.map_image = Image.open("./sample_assets/sample_world_map.jpg")
-        
-        path = Path(file_name).parents[0]
+        #store the file name of the image in the class instance
+        #we arent assuming it is a relative or absolute path
+        self.file_name = file_name
+
+        #get the absolute path for the map image
+        self.image_file = pathlib.Path(self.file_name)
+
+        #get the absolute path the json file (rules are same file name same directory)
+        self.json_file = pathlib.Path(self.image_file.parent,self.image_file.stem+".json")
+
+        #if the json file doesnt exist yet initialize it
+        if not os.path.isfile(self.json_file):
+            #initial json data
+            #this needs to contain whatever is read below
+            initial_json_data = {
+                "image_file":self.image_file.name,
+                "width_feet":1,
+                "height_feet":1
+            }
+            with open(self.json_file, "w+") as f:
+                json.dump(initial_json_data,f)
 
 
-        #get the width and height out of the json file
-        with open(file_name) as f:
+
+
+        #get the map image file name (dont know if we even need that
+        # and width and height in feet out of the json file
+        with open(self.json_file) as f:
             data = json.load(f)
-            self.image_file_name = self.image_file_name = Path(path, data["image_file_name"])
+            self.image_file_name = data["image_file"]
             self.map_width_feet = data["width_feet"]
-            self.map_height_feet  = data["height_feet"]
+            self.map_height_feet = data["height_feet"]
 
 
-        self.map_image = Image.open(self.image_file_name)
+
+        self.map_image = Image.open(self.image_file)
         #self.map_width_feet = 9950000
         #self.map_height_feet  = 7550000
 
@@ -75,13 +91,14 @@ class Map():
         #get the photoimage after transformations
         self.map = ImageTk.PhotoImage(self.map_image)
 
-        #self.canvas.create_image(self.canvas.winfo_screenwidth()/2, self.canvas.winfo_screenheight()/2,image=self.map)
-
 
         #now that the map is created and resized, calculate feet pet pixel
-        #could use height or width, using an average of the two for now incase ratios are off
-        self.map_feet_per_pixel = ((self.map_height_feet / self.map_image.height) + (self.map_width_feet / self.map_image.width))/2
+        #just using the width for this... height or width should both work
+        #needs to be stored as one of the file dimensions instad of just 
+        #having feet_per_pixel in the json file because of different screen resolutions
+        self.map_feet_per_pixel = self.map_width_feet / self.map_image.width
 
 
+    #simple method to just draw the map
     def draw_map(self):
         self.canvas.create_image(self.canvas.winfo_screenwidth()/2, self.canvas.winfo_screenheight()/2,image=self.map)
